6e4aeef0ff7a7ff974a8347f28ab8be3ad81e33a
rezenderegis <rezenderegis@gmail.com>
2022-04-17 11:50:57 -0300

    List Products

---
+++ src/modules/products/services/ListProductService.ts
@@ -0,0 +1,20 @@
+import { getCustomRepository, getRepository } from "typeorm";
+import Product from "../typeorm/entities/Products";
+import { ProductRepository } from "../typeorm/repositories/ProductsRepository";
+
+
+class ListProductService {
+
+    public async execute (): Promise<Product[]> {
+
+        const productsRepository = getCustomRepository(ProductRepository);
+
+        const products = productsRepository.find();
+
+        return products;
+        
+    }
+
+
+}
+export default ListProductService;
