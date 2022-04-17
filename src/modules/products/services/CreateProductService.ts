989266707847db43147f9e1c3e35c3c619f51045
rezenderegis <rezenderegis@gmail.com>
2022-04-17 15:22:27 -0300

    Finishing

--- src/modules/products/services/CreateProductService.ts
+++ src/modules/products/services/CreateProductService.ts
@@ -12,21 +12,18 @@ interface IRequest {
 class CreateProductService {
 
     public async execute({name, price, quantity}: IRequest ): Promise<Product> {
-        console.log('Salvando############################3');
 
         const productsRepository = getCustomRepository(ProductRepository);
-       // const productExists = await productsRepository.findByName(name);
-        //if (productExists) {
-         //   throw new AppError('There is already one product with this name');
-        //}
-        console.log('Salvando############################4');
+        const productExists = await productsRepository.findByName(name);
+        if (productExists) {
+           throw new AppError('There is already one product with this name');
+        }
 
         const product = productsRepository.create({
             name,
             price,
             quantity,
         });
-        console.log('Salvando############################5');
         console.log(product);
 
 
