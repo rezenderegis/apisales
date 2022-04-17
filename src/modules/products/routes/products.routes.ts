41ba38f94dd25deded444af84635b1f876f4b649
rezenderegis <rezenderegis@gmail.com>
2022-04-17 16:36:33 -0300

    Validation with Celebrate and Joi.

--- src/modules/products/routes/products.routes.ts
+++ src/modules/products/routes/products.routes.ts
@@ -1,14 +1,47 @@
 import {Router} from 'express';
 import ProducsController from '../controllers/ProductsControler';
+import {celebrate,Joi, Segments} from 'celebrate';
 import { ProductRepository } from '../typeorm/repositories/ProductsRepository';
 const productsRouter = Router();
 const productsController = new ProducsController();
 
 productsRouter.get('/', productsController.index);
-productsRouter.get('/:id', productsController.show);
-productsRouter.post('/', productsController.create);
-productsRouter.put('/:id', productsController.update);
-productsRouter.delete('/:id', productsController.delete);
+
+/*Included celebrate to validate request. Middleware on celebrate function
+Import here and server.ts
+*/
+productsRouter.get('/:id',
+celebrate({
+    [Segments.PARAMS]: {id:Joi.string().uuid().required(),}
+}),
+productsController.show);
+
+
+productsRouter.post('/', 
+celebrate({[Segments.BODY]: {
+    name: Joi.string().required(),
+    price: Joi.number().precision(2).required(),
+    quantity: Joi.number().required(),
+    }
+}),
+productsController.create);
+
+productsRouter.put('/:id', 
+    celebrate({
+        [Segments.BODY]: {
+            name: Joi.string().required(),
+            price: Joi.number().precision(2).required(),
+            quantity: Joi.number().required(),
+        },
+        [Segments.PARAMS]: { id: Joi.string().uuid().required(), }
+    }),
+productsController.update);
+productsRouter.delete('/:id', 
+    celebrate({
+        [Segments.PARAMS]: { id: Joi.string().uuid().required(), }
+    }),
+
+productsController.delete);
 
 /* productsRouter is a file with all routes. We need to import this file
 in main route file index.ts
