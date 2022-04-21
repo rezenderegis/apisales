cb8a58a71f19273533132d58cbcb3719406171d9
rezenderegis <rezenderegis@gmail.com>
2022-04-21 14:41:54 -0300

    Token

--- src/modules/users/services/CreationSessionService.ts
+++ src/modules/users/services/CreationSessionService.ts
@@ -4,7 +4,7 @@ import { Tracing } from "trace_events"
 import { getCustomRepository } from "typeorm";
 import User from "../typeorm/entities/Users";
 import UsersRepository from "../typeorm/repositories/UserRepository";
-
+import {sign} from 'jsonwebtoken';
 
 interface IRequest {
     email: string;
@@ -12,6 +12,10 @@ interface IRequest {
 
 }
 
+interface IResponse {
+    user: User;
+    token: string;
+}
 class CreateSessionsService {
 
 
@@ -31,7 +35,22 @@ class CreateSessionsService {
             throw new AppError('Incorrect emaill/password combination.', 401);
         }
 
-        return user;
+        //After pass this two steps, configure JWT. Include sign method
+
+        /*Three parameters: 
+        1. payload o send user (not use personal data),include 
+        something like ID to use. Something to identificate user. 
+        2. Hash - secrete use to create. Include every informatio or md5 hash
+        */
+        
+        const token = sign({}, '7456fd7a4c7885ab2066c5fc3bb99872', {
+            subject: user.id,
+            expiresIn: '1d',
+        });
+        return {
+            user, 
+            token
+        };
     }
 
 
